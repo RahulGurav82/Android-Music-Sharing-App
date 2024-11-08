68ce698fbda3a6abb3c0b7a6aa202dc7098d9e00
Rahul Guarv <126690105+rahul428742@users.noreply.github.com>
2024-11-08 14:51:24 +0530

    admin3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = process.env.PORT || 3000;
 
